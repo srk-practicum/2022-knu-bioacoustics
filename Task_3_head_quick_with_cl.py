52041c4054335f9a87a14638c25ccb9cda9b9df0
Oomycota145 <89969538+Oomycota145@users.noreply.github.com>
2022-02-11 00:37:04 +0200

    Update Task_3_head_quick_with_cl.py

--- Task_3_head_quick_with_cl.py
+++ Task_3_head_quick_with_cl.py
@@ -1,3 +1,6 @@
+
+
+
 import librosa
 import pandas as pd
 import random 
@@ -100,59 +103,109 @@ clusters = np.unique(yhat)
 for cluster in clusters:
 	row_ix = np.where(yhat == cluster)
 	plt.scatter(X[row_ix, 0], X[row_ix, 1])
+all_right=0
+FAR=0
+FRR=0
+for i in range(len(yhat)):
+   all_right+=(yhat[i]==y[i])
+   FAR+=(yhat[i]==1 and y[i]==0)
+   FRR+=(yhat[i]==0 and y[i]==1)        
+print('If using Affinity Propagation: ok={} FAR={} FRR={}'.format(all_right,FAR,FRR))
 
 f2=plt.subplot(3,2,2)
 f2.set_title('Using DBSCAN Clustering')
-model = DBSCAN(eps=0.30, min_samples=4)
-yhat = model.fit_predict(X)
+model = DBSCAN(eps=0.30, min_samples=2)
+yhat2 = model.fit_predict(X)
 clusters = np.unique(yhat)
 for cluster in clusters:
-	row_ix = np.where(yhat == cluster)
+	row_ix = np.where(yhat2 == cluster)
 	plt.scatter(X[row_ix, 0], X[row_ix, 1])
+all_right=0
+FAR=0
+FRR=0
+for i in range(len(yhat2)):
+   all_right+=(yhat2[i]==y[i])
+   FAR+=(yhat2[i]==1 and y[i]==0)
+   FRR+=(yhat2[i]==0 and y[i]==1)        
+print('If using DBSCAN Clustering: ok={} FAR={} FRR={}'.format(all_right,FAR,FRR))
 
 f3=plt.subplot(3,2,3)
 f3.set_title('Using K-Means Clustering')
-model = KMeans(n_clusters=4)
+model = KMeans(n_clusters=2)
 model.fit(X)
-yhat = model.predict(X)
-clusters = np.unique(yhat)
+yhat3 = model.predict(X)
+clusters = np.unique(yhat3)
 for cluster in clusters:
-	row_ix = np.where(yhat == cluster)
+	row_ix = np.where(yhat3 == cluster)
 	plt.scatter(X[row_ix, 0], X[row_ix, 1])
+	#print(X[row_ix,0], X[row_ix,1])
+all_right=0
+FAR=0
+FRR=0
+for i in range(len(yhat3)):
+   all_right+=(yhat3[i]==y[i])
+   FAR+=(yhat3[i]==1 and y[i]==0)
+   FRR+=(yhat3[i]==0 and y[i]==1)        
+print('If using K-Means Clustering: ok={} FAR={} FRR={}'.format(all_right,FAR,FRR))
+
 labels = model.labels_
-print(metrics.silhouette_score(X, labels, metric='euclidean'))
-print(davies_bouldin_score(X, labels))
+#print(metrics.silhouette_score(X, labels, metric='euclidean'))
+#print(davies_bouldin_score(X, labels))
 
 f4=plt.subplot(3,2,4)
 f4.set_title('Using OPTICS Clustering')
 model = OPTICS(eps=0.8, min_samples=4)
-yhat = model.fit_predict(X)
-clusters = np.unique(yhat)
+yhat4 = model.fit_predict(X)
+clusters = np.unique(yhat4)
 for cluster in clusters:
-	row_ix = np.where(yhat == cluster)
+	row_ix = np.where(yhat4 == cluster)
 	plt.scatter(X[row_ix, 0], X[row_ix, 1])
+all_right=0
+FAR=0
+FRR=0
+for i in range(len(yhat4)):
+   all_right+=(yhat4[i]==y[i])
+   FAR+=(yhat4[i]==1 and y[i]==0)
+   FRR+=(yhat4[i]==0 and y[i]==1)        
+print('If using OPTICS Clustering: ok={} FAR={} FRR={}'.format(all_right,FAR,FRR))
 
 f5=plt.subplot(3,2,5)
 f5.set_title('Using Spectra Clustering')
-model = KMeans(n_clusters=4)
-yhat = model.fit_predict(X)
-clusters = np.unique(yhat)
+model = KMeans(n_clusters=2)
+yhat5 = model.fit_predict(X)
+clusters = np.unique(yhat5)
 for cluster in clusters:
-	row_ix = np.where(yhat == cluster)
-	plt.scatter(X[row_ix, 0], X[row_ix, 1])
+	row_ix = np.where(yhat5 == cluster)
+	plt.scatter(X[row_ix, 128], X[row_ix, 141])
+all_right=0
+FAR=0
+FRR=0
+for i in range(len(yhat5)):
+   all_right+=(yhat5[i]*y[i]!=0)
+   FAR+=(yhat5[i]!=0 and y[i]==0)
+   FRR+=(yhat5[i]==0 and y[i]!=0)        
+print('If using Spectra Clustering: ok={} FAR={} FRR={}'.format(all_right,FAR,FRR))
 
 
 f6=plt.subplot(3,2,6)
 f6.set_title('Using Gaussian Mixture Clustering')
-model = GaussianMixture(n_components=4)
+model = GaussianMixture(n_components=2)
 model.fit(X)
-yhat = model.predict(X)
-clusters = np.unique(yhat)
+yhat6 = model.predict(X)
+clusters = np.unique(yhat6)
 for cluster in clusters:
-	row_ix = np.where(yhat == cluster)
-	plt.scatter(X[row_ix, 0], X[row_ix, 1])
-	print(X[row_ix, 0], X[row_ix, 1])
-print(":)")
+	row_ix = np.where(yhat6 == cluster)
+	plt.scatter(X[row_ix, 140], X[row_ix, 14])
+	
+all_right=0
+FAR=0
+FRR=0
+for i in range(len(yhat6)):
+   all_right+=(yhat6[i]==y[i])
+   FAR+=(yhat6[i]==1 and y[i]==0)
+   FRR+=(yhat6[i]==0 and y[i]==1)        
+print('If using Gaussian Mixture Clustering: ok={} FAR={} FRR={}'.format(all_right,FAR,FRR))
+
 plt.show()
 
 
